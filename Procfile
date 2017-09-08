92ab2f2cfbfaecfc4b8d4a16bb3bda69b70c46ce
Saad Yousaf <saad.yousaf@arbisoft.com>
2017-09-08 11:21:44 +0500

    Additional files for heroku.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn autoshop.wsgi --log-file -
--- autoshop/private.py
+++ autoshop/private.py
@@ -3,18 +3,18 @@ import os
 from autoshop.settings import BASE_DIR
 
 
-DEBUG = True
+# DEBUG = True
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 # Development SQLITE3 database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # # Development POSTGRESQL database
 # DATABASES = {
